3c567c6e9ab7dc7ffe790ef27848bfc88b5d120e
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-21 10:35:36 +1000

    Minor.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PuppeteerHandlerModule } from './puppeteer-handler/puppeteer-handler.module';
 
 @Module({
-  imports: [PuppeteerHandlerModule],
+  imports: [
+    PuppeteerHandlerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
